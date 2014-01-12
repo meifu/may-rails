8566171ab37b779885b5cdf833fff1429c9378cf
meifu <meifutoolate@gmail.com>
2014-01-12 20:10:35 +0800

    first time commit on master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MayRails::Application.config.secret_key_base = '5adc48de78143e09e0ea54a6a106807a9f4d528e35e9885a11ad99c79e86bcee5e1575e09034ca15278ca46b5e49c184805dec30865df00a649d324907f0781c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MayRails::Application.config.session_store :cookie_store, key: '_may-rails_session'
